3687a9699abb55759cc54deabad181bfcc68f55a
Daaavid <d4v1dr4m0s@gmail.com>
2025-09-18 20:19:32 -0300

    XD

--- TP2/src/Grafo/Grafo.java
+++ TP2/src/Grafo/Grafo.java
@@ -1,5 +1,18 @@
 package Grafo;
 
-public class Grafo {
+import java.util.ArrayList;
+import java.util.List;
+import Usuario.Usuario;
 
+public class Grafo {
+    private List<Usuario> vertices;
+
+    public Grafo(int capacidadInicial) {
+        vertices = new ArrayList<>(capacidadInicial);
+    }
+
+    public void AgregarVertice(Usuario u) {
+        vertices.add(u);
+        System.out.println("Agregado: " + u);
+    }
 }
--- TP2/src/Usuario/Usuario.java
+++ TP2/src/Usuario/Usuario.java
@@ -2,12 +2,32 @@ package Usuario;
 
 public class Usuario {
     private int interesTango, interesFolklore, interesRock, interesUrbano;
-    public Usuario (int interesTango, int interesFolklore, int interesRock, int interesUrbano){
-        if(!parametroValido(interesTango) && !parametroValido(interesFolklore) && parametroValido(interesRock) && parametroValido(interesUrbano)){
 
-        }
+    public Usuario (int interesTango, int interesFolklore, int interesRock, int interesUrbano){
+        this.interesTango = validar(interesTango);
+        this.interesFolklore = validar(interesFolklore);
+        this.interesRock = validar(interesRock);
+        this.interesUrbano = validar(interesUrbano);
     }
-}
-private boolean parametroValido(int i){
-    return false;
+
+    private int validar(int i){
+        if(i < 1 || i > 5) {
+            System.out.println("⚠ Valor inválido (" + i + "), se asigna 1 por defecto.");
+            return 1;
+        }
+        return i;
+    }
+
+    public int getTango() { return interesTango; }
+    public int getFolklore() { return interesFolklore; }
+    public int getRock() { return interesRock; }
+    public int getUrbano() { return interesUrbano; }
+
+    @Override
+    public String toString() {
+        return "[Tango=" + interesTango + 
+               ", Folklore=" + interesFolklore +
+               ", Rock=" + interesRock +
+               ", Urbano=" + interesUrbano + "]";
+    }
 }
