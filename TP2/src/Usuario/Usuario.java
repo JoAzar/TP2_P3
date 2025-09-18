d3b155872c37450b7c030dbae537a7e2a863c92b
Daaavid <d4v1dr4m0s@gmail.com>
2025-09-18 19:25:50 -0300

    Para comenzar

---
+++ TP2/src/Usuario/Usuario.java
@@ -0,0 +1,13 @@
+package Usuario;
+
+public class Usuario {
+    private int interesTango, interesFolklore, interesRock, interesUrbano;
+    public Usuario (int interesTango, int interesFolklore, int interesRock, int interesUrbano){
+        if(!parametroValido(interesTango) && !parametroValido(interesFolklore) && parametroValido(interesRock) && parametroValido(interesUrbano)){
+
+        }
+    }
+}
+private boolean parametroValido(int i){
+    return false;
+}
