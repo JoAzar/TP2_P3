cabcc344aa2ffc83a29badc1a985d5bdf6626585
Daaavid <d4v1dr4m0s@gmail.com>
2025-09-18 21:01:07 -0300

    Comenzamo mi gente
    XD

--- TP2/src/Interfaz/Main.java
+++ TP2/src/Interfaz/Main.java
@@ -10,7 +10,7 @@ public class Main {
         Usuario u1= new Usuario(4,4,4,4);
         Usuario u2= new Usuario(1,5,1,3);
         Usuario u3= new Usuario(3,2,4,5);
-        Usuario u4= new Usuario(-1,4,2,4);  
+        Usuario u4= new Usuario(56,4,2,4);  
         Grafo g = new Grafo(4);
         g.AgregarVertice(u1);
         g.AgregarVertice(u2);
