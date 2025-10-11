153e1a8508345a67932c4c1185d1bf4ae105b749
Daaavid <d4v1dr4m0s@gmail.com>
2025-10-11 18:01:11 -0300

    Update Grafotest.java

--- TP2/src/Test/Grafotest.java
+++ TP2/src/Test/Grafotest.java
@@ -3,6 +3,7 @@ package Test;
 import org.junit.Test;
 import model.Usuario;
 import model.Grafo;
+import usuario;
 
 public class Grafotest {
 
@@ -14,8 +15,34 @@ public class Grafotest {
 	
 	@Test(expected = IllegalArgumentException.class)
     public void crearUsuarioInvalido(){
-        Grafo grafo= new Grafo();
-        new Usuario(null, null);
+        Usuario usuario1= new Usuario(null, null);
 	}
 
+
+	@Test(expected = IllegalArgumentException.class)
+    public void crearUsuarioParámetrosGustoInválidos(){
+       Map<String, Integer> gustos= new HashMap<String, Integer>("rock": -2, "jazz": 3, "folklore":0, "urbano":5);
+	   Usuario usuario1 = new usuario("juan", gustos);
+	}
+
+
+	@Test
+	public void calcularAfinidadTest(){
+		Map<String, Integer> gustos1= new HashMap<String, Integer>("rock": 2, "jazz": 3, "folklore":2, "urbano":5);
+		Map<String, Integer> gustos2= new HashMap<String, Integer>("rock": 3, "jazz": 4, "folklore":1, "urbano":5);
+
+		Usuario usuario1= new Usuario("Juan", gustos1);
+		Usuario usuario2= new Usuario("Pedro", gustos2);
+
+		int afinidad= usuario1.afinidadCon(usuario2);
+		assertTrue(afinidad==3);
+	}
+
+	@Test
+	public void algoritmoAGMTest(){
+		
+		Grafo test= new Grafo().algoritmoAGM();
+	}
+	
+	
 }
