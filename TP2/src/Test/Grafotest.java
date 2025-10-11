55fda2404f178582cb609a8b8a9371593c173942
Daaavid <d4v1dr4m0s@gmail.com>
2025-10-11 20:18:19 -0300

    TESTSSSS

--- TP2/src/Test/Grafotest.java
+++ TP2/src/Test/Grafotest.java
@@ -1,48 +1,101 @@
 package Test;
 
+import static org.junit.Assert.*;
+import java.util.*;
 import org.junit.Test;
-import model.Usuario;
 import model.Grafo;
-import usuario;
+import model.Usuario;
+import model.Arista;
 
-public class Grafotest {
+public class GrafoTest {
 
-	@Test(expected = IllegalArgumentException.class)
-	public void agregarVerticeNulo() {
-		Grafo grafo = new Grafo();
-		grafo.agregarVertice(null);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-    public void crearUsuarioInvalido(){
-        Usuario usuario1= new Usuario(null, null);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void agregarVerticeNuloTest() {
+        Grafo grafo = new Grafo();
+        grafo.agregarVertice(null);
+    }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void crearUsuarioNuloTest() {
+        new Usuario(null, null);
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-    public void crearUsuarioParámetrosGustoInválidos(){
-       Map<String, Integer> gustos= new HashMap<String, Integer>("rock": -2, "jazz": 3, "folklore":0, "urbano":5);
-	   Usuario usuario1 = new usuario("juan", gustos);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void crearUsuarioSinGustosTest() {
+        Map<String, Integer> gustos = new HashMap<>();
+        new Usuario("Juan", gustos);
+    }
 
+    @Test
+    public void calcularAfinidadTest() {
+        Map<String, Integer> gustos1 = new HashMap<>();
+        gustos1.put("rock", 2);
+        gustos1.put("jazz", 3);
+        gustos1.put("folklore", 2);
+        gustos1.put("urbano", 5);
 
-	@Test
-	public void calcularAfinidadTest(){
-		Map<String, Integer> gustos1= new HashMap<String, Integer>("rock": 2, "jazz": 3, "folklore":2, "urbano":5);
-		Map<String, Integer> gustos2= new HashMap<String, Integer>("rock": 3, "jazz": 4, "folklore":1, "urbano":5);
+        Map<String, Integer> gustos2 = new HashMap<>();
+        gustos2.put("rock", 3);
+        gustos2.put("jazz", 4);
+        gustos2.put("folklore", 1);
+        gustos2.put("urbano", 5);
 
-		Usuario usuario1= new Usuario("Juan", gustos1);
-		Usuario usuario2= new Usuario("Pedro", gustos2);
+        Usuario usuario1 = new Usuario("Juan", gustos1);
+        Usuario usuario2 = new Usuario("Pedro", gustos2);
 
-		int afinidad= usuario1.afinidadCon(usuario2);
-		assertTrue(afinidad==3);
-	}
+        int afinidad = usuario1.afinidadCon(usuario2);
+        assertEquals(3, afinidad);
+    }
 
-	@Test
-	public void algoritmoAGMTest(){
-		
-		Grafo test= new Grafo().algoritmoAGM();
-	}
-	
-	
+    @Test
+    public void agregarVerticeTest() {
+        Grafo grafo = new Grafo();
+        Map<String, Integer> gustos = new HashMap<>();
+        gustos.put("rock", 5);
+        Usuario usuario = new Usuario("Lucas", gustos);
+
+        grafo.agregarVertice(usuario);
+        assertTrue(grafo.getVertices().contains(usuario));
+        assertEquals(1, grafo.getVertices().size());
+    }
+
+    @Test
+    public void agregarAristaTest() {
+        Grafo grafo = new Grafo();
+
+        Map<String, Integer> gustos = new HashMap<>();
+        gustos.put("metal", 5);
+
+        Usuario u1 = new Usuario("Juan", gustos);
+        Usuario u2 = new Usuario("Pedro", gustos);
+
+        grafo.agregarVertice(u1);
+        grafo.agregarVertice(u2);
+        grafo.agregarArista(u1, u2, 10);
+
+        assertEquals(1, grafo.getAristas().size());
+        Arista arista = grafo.getAristas().get(0);
+        assertEquals(u1, arista.getOrigen());
+        assertEquals(u2, arista.getDestino());
+        assertEquals(10, arista.getPeso());
+    }
+
+    @Test
+    public void AgregarAristaDuplicadaTest() {
+        Grafo grafo = new Grafo();
+
+        Map<String, Integer> gustos = new HashMap<>();
+        gustos.put("metal", 5);
+
+        Usuario u1 = new Usuario("Juan", gustos);
+        Usuario u2 = new Usuario("Pedro", gustos);
+
+        grafo.agregarVertice(u1);
+        grafo.agregarVertice(u2);
+
+        grafo.agregarArista(u1, u2, 10);
+        grafo.agregarArista(u1, u2, 20);
+
+        assertEquals(1, grafo.getAristas().size());
+    }
 }
